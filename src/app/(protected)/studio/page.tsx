3d240f7dd2c8765a138bf78b3d1e5b686e83b7df
Devraj Songara <devrajsongara894@gmail.com>
2026-03-14 19:29:40 +0530

    feat: reuse ProfileTabs component for studio mode

--- src/app/(protected)/studio/page.tsx
+++ src/app/(protected)/studio/page.tsx
@@ -1,15 +1,18 @@
 import ProfileTabs from '@/components/profile/ProfileTabs'
 import StudioLayout from '@/components/studio/StudioLayout'
+import { playlists } from '@/data/playlists'
 import { studioStats } from '@/data/studio'
+import { tweets } from '@/data/tweets'
+import { videos } from '@/data/videos'
 
 export default function StudioPage() {
   return (
     <StudioLayout stats={studioStats}>
       <ProfileTabs
-        videos={[]}
-        playlists={[]}
-        tweets={[]}
-        subscribedChannels={[]}
+        mode="studio"
+        videos={videos}
+        playlists={playlists}
+        tweets={tweets}
       />
     </StudioLayout>
   )
--- src/components/profile/ProfileTabs.tsx
+++ src/components/profile/ProfileTabs.tsx
@@ -7,28 +7,45 @@ import { homeVideoType } from '@/types/videos'
 import { playlistType } from '@/types/playlist'
 import { tweetTypes } from '@/types/tweet'
 import { subscribedChannelTypes } from '@/types/subscribedChannel'
+import { Button } from '../ui/button'
 
 interface profileTabsProps {
+  mode?: 'profile' | 'studio'
   videos: homeVideoType[]
   playlists: playlistType[]
   tweets: tweetTypes[]
-  subscribedChannels: subscribedChannelTypes[]
+  subscribedChannels?: subscribedChannelTypes[]
 }
 
 export default function ProfileTabs({
   videos,
   playlists,
   tweets,
-  subscribedChannels,
+  subscribedChannels = [],
+  mode = 'profile',
 }: profileTabsProps) {
   return (
     <Tabs defaultValue="videos" className="mt-6 px-8">
-      <TabsList className="flex justify-center md:space-x-48 bg-[#654c7640]">
-        <TabsTrigger value="videos">Videos</TabsTrigger>
-        <TabsTrigger value="playlists">Playlists</TabsTrigger>
-        <TabsTrigger value="tweets">Tweets</TabsTrigger>
-        <TabsTrigger value="subscribed">Subscribed</TabsTrigger>
-      </TabsList>
+      {mode === 'profile' ? (
+        <TabsList className="flex justify-center md:space-x-48 bg-[#654c7640]">
+          <TabsTrigger value="videos">Videos</TabsTrigger>
+          <TabsTrigger value="playlists">Playlists</TabsTrigger>
+          <TabsTrigger value="tweets">Tweets</TabsTrigger>
+          <TabsTrigger value="subscribed">Subscribed</TabsTrigger>
+        </TabsList>
+      ) : (
+        <div className="flex items-center justify-between bg-[#654c7640] rounded-lg">
+          <TabsList className="md:gap-8 bg-transparent">
+            <TabsTrigger value="videos">Videos</TabsTrigger>
+            <TabsTrigger value="playlists">Playlists</TabsTrigger>
+            <TabsTrigger value="tweets">Tweets</TabsTrigger>
+          </TabsList>
+
+          <Button className="bg-purple-600 px-4 rounded-lg text-sm font-semibold">
+            Upload
+          </Button>
+        </div>
+      )}
 
       <TabsContent value="videos">
         <VideosTab videos={videos} />
@@ -42,9 +59,11 @@ export default function ProfileTabs({
         <TweetsTab tweets={tweets} />
       </TabsContent>
 
-      <TabsContent value="subscribed">
-        <SubscribedTab subscribedChannels={subscribedChannels} />
-      </TabsContent>
+      {mode === 'profile' && (
+        <TabsContent value="subscribed">
+          <SubscribedTab subscribedChannels={subscribedChannels} />
+        </TabsContent>
+      )}
     </Tabs>
   )
 }
