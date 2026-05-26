6b03173cee68cb8dda74d2d7c85ba5d3267a72c3
Devraj Songara <devrajsongara894@gmail.com>
2026-05-26 15:20:26 +0530

    feat(video): implement subscription and like toggle functionality with optimistic updates

--- src/components/video/VideoMeta.tsx
+++ src/components/video/VideoMeta.tsx
@@ -5,12 +5,69 @@ import { formatTimeAgo } from '@/lib/utils'
 import { VideoDetails } from '@/types/videos.types'
 import { ThumbsUp, Share2, Download } from 'lucide-react'
 import Link from 'next/link'
+import { useState } from 'react'
+import { toggleSubscriptions } from '@/services/subscriptions' 
+import { toggleVideoLike } from '@/services/likes'
 
 interface VideoMetaProps {
   videoDetails: VideoDetails
 }
 
 export default function VideoMeta({ videoDetails }: VideoMetaProps) {
+  const [isSubscribed, setIsSubscribed] = useState(videoDetails.owner.isSubscribed)
+  const [loadingToggleSubscription, setLoadingToggleSubscription] = useState(false)
+
+  const [totalLikes, setTotalLikes] = useState(videoDetails.likesCount)
+  const [isLiked, setIsLiked] = useState(videoDetails.isLiked)
+  const [loadingToggleLike, setLoadingToggleLike] = useState(false)
+
+  const handleToggleSubscription = async () => {
+    try {
+      setLoadingToggleSubscription(true)
+      setIsSubscribed((prev) => !prev)
+      await toggleSubscriptions(videoDetails.owner._id)
+
+    } catch (error) {
+      setIsSubscribed((prev) => !prev)
+      console.error("Failed to toggle subscription: ", error)
+
+    } finally {
+        setLoadingToggleSubscription(false)
+    }
+  }
+
+  const handleToggleLike = async () => {
+    try {
+      setLoadingToggleLike(true)
+
+      // optimistic update
+      if (isLiked) {
+        setTotalLikes((prev) => prev - 1)
+      } else {
+        setTotalLikes((prev) => prev + 1)
+      }
+
+      setIsLiked((prev) => !prev)
+
+      await toggleVideoLike(videoDetails._id)
+
+    } catch (error) {
+      // rollback
+      if (isLiked) {
+        setTotalLikes((prev) => prev + 1)
+      } else {
+        setTotalLikes((prev) => prev - 1)
+      }
+
+      setIsLiked((prev) => !prev)
+
+      console.error("Failed to toggle like: ", error)
+
+    } finally {
+      setLoadingToggleLike(false)
+    }
+  }
+
   return (
     <div className="space-y-4">
       {/* Title */}
@@ -38,24 +95,43 @@ export default function VideoMeta({ videoDetails }: VideoMetaProps) {
             </p>
           </div>
 
-          <Button variant="secondary" className="ml-4 rounded-full">
-            {videoDetails.isOwner ? (
-              <Link href={`/studio`}>
+          {videoDetails.isOwner ? (
+            <Button variant="secondary" className="ml-4 rounded-full" asChild>
+              <Link href="/studio">
                 Manage Channel
               </Link>
-            ) : (
-              <Button variant="secondary" className="rounded-full">
-                {videoDetails.owner.isSubscribed ? "Subscribed" : "Subscribe"}
-              </Button>
-            )}
-          </Button>
+            </Button>
+          ) : (
+            <Button
+              onClick={handleToggleSubscription}
+              disabled={loadingToggleSubscription}
+              variant="secondary"
+              className="ml-4 rounded-full"
+            >
+              {loadingToggleSubscription
+                ? "Loading..."
+                : isSubscribed
+                  ? "Subscribed"
+                  : "Subscribe"}
+            </Button>
+          )}
         </div>
 
         {/* Action Buttons */}
         <div className="flex gap-2 shrink-0">
-          <Button variant="secondary" size="sm" className="gap-2">
-            <ThumbsUp className="h-4 w-4" />
-            {videoDetails.likesCount}
+         <Button
+            variant={isLiked ? "default" : "secondary"}
+            size="sm"
+            className="gap-2"
+            onClick={handleToggleLike}
+            disabled={loadingToggleLike}
+          >
+            <ThumbsUp
+              className={`h-4 w-4 transition-all ${
+                isLiked ? "fill-current" : ""
+              }`}
+            />
+            {totalLikes}
           </Button>
           <Button variant="secondary" size="sm" className="gap-2">
             <Share2 className="h-4 w-4" />
